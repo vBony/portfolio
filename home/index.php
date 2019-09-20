05d711921878b32454c2277abb5f2c0b09ce7606
Vinicius <vini_07-ferreira@hotmail.com>
2019-09-19 22:00:04 -0300

    Primeiro commit: Foi feito o cabeçalho, apresentação e parte do conteudo central.

---
+++ home/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Vinicius: Portifólio</title>
+    <meta name="author" content="Vinicius de Assis Ferreira">
+    <meta http-equiv="content-language" content="pt-br, en-US, fr" />
+    <meta name="copyright" content="© 2019 Vinicius F." />
+    <link rel="stylesheet" href="index.css">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
+    <link href="https://fonts.googleapis.com/css?family=Staatliches&display=swap" rel="stylesheet"> 
+
+</head>
+
+<body>
+<div id="mainbody">
+<!-- CABEÇALHO COMEÇA AQUI -->
+<header>
+    <div id="cabecalho">
+        <div id="cabecalhoinv">
+            <div id="menu">
+                <div class="menuobj" id="exobj">Sobre</div>
+                <div class="menuobj">Projetos</div>
+                <div class="menuobj">Resumo</div>
+                <div class="menuobj">Contato</div>
+            </div>
+        </div>
+    </div>
+
+</header>
+<!-- CABEÇALHO TERMINA AQUI -->
+
+<!-- SUB CONTENT COMEÇA AQUI -->
+<div id="subcontent">
+    <div id="subcontentinv">
+        
+        <div id="foto">
+            <img src="imgs/tempphoto" alt="" id="ofoto">
+        </div>
+
+        <div id="conttext">
+            <div id="text1">What is Lorem Ipsum?</div>
+            <div id="textname">Lore Ipsum</div>
+            <div id="text3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Cras in magna ornare, interdum lorem sit amet, congue libero.
+                Mauris quis lectus quis quam commodo bibendum. Suspendisse
+                a maximus ante. Morbi id erat quis libero maximus fringilla.
+                Mauris vulputate libero nec dolor imperdiet elementum.
+                Mauris nibh enim, fermentum quis ipsum in, malesuada
+                placerat ipsum. Morbi convallis dapibus diam et
+                sollicitudin. Etiam vel augue suscipit metus posuere commodo.
+            </div>
+            <div id="btncontrato">Contrate-me</div>
+        </div>
+    </div>
+</div>
+<!-- SUB CONTENT TERMINA AQUI -->
+
+<div id="midcontent">
+    <div id="overview">
+        <div id="OVinv">
+            <div id="OVtitle">Visão Geral Técnica</div>
+            <div id="OVtext">
+            Lorem Ipsum is simply dummy text of the printing and typesetting 
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type 
+            and scrambled it to make a type specimen book. It has survived not 
+            only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with 
+            the release of Letraset sheets containing Lorem Ipsum passages, and 
+            more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+            </div>
+        </div>
+    </div>
+
+
+    <div id="listc">
+        <div id="listcinv">
+            <div id="box-content">
+                <div class="icon"><img id="imgicon" src="imgs/webpicon.jpg" alt=""></i></div>
+                <div id="box-title">Conhecimentos Gerais</div>
+
+                    <div class="linhaOV" id="OVlinha1">
+                        <div><img class="imgchecked" src="imgs/checked.png" alt=""></div>
+                        <div class="textOV">PHP</div>
+                    </div>
+
+                    <div class="linhaOV" id="OVlinha1">
+                        <div><img class="imgchecked" src="imgs/checked.png" alt=""></div>
+                        <div class="textOV">Javascript</div>
+                    </div>
+
+                    <div class="linhaOV" id="OVlinha1">
+                        <div><img class="imgchecked" src="imgs/checked.png" alt=""></div>
+                        <div class="textOV">Jquery</div>
+                    </div>
+
+                    <div class="linhaOV" id="OVlinha1">
+                        <div><img class="imgchecked" src="imgs/checked.png" alt=""></div>
+                        <div class="textOV">HTML/CSS</div>
+                    </div>
+
+                    <div class="linhaOV" id="OVlinha1">
+                        <div><img class="imgchecked" src="imgs/checked.png" alt=""></div>
+                        <div class="textOV">MySQL</div>
+                    </div>
+
+            </div>
+            
+        </div>
+    </div>
+</div>
+    
+    
+    
+    
+</div>
+</body>
+
+</html>

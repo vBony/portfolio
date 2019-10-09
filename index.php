ac4305b7accc5c9889b60767b86445b63efde814
Vinicius <vini_07-ferreira@hotmail.com>
2019-10-09 18:23:34 -0300

    Os botões no inicio do site agora está funcionando. // Adicionado duas seções no corpo do site (contatos e Sobre mim com texto temporario)

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Vinicius de A. Ferreira - Portfólio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="frameworks/bootstrap.min.css">
+    <script type="text/javascript" src="frameworks/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="frameworks/bootstrap.min.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="styles/VF.ico">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet"> 	
+    <link rel="stylesheet" href="styles/index.css">
+    <meta name="copyright" content="© 2019 Vinicius de A. Ferreira" />
+    <script type="text/javascript" src="javascript/script.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<header>
+    <div id="header-background" class="container-fluid">
+        <div id="contentheader">
+            <div id="img"><img src="imgs/avatar.png" alt=""></div>
+            <div id="personaldata">
+                <div id="name">Vinicius de Assis</div>
+                <div id="profissao">Desenvolvedor WEB</div>
+                <div id="contacts">
+                    <div id="facebookicon"><a href="https://www.facebook.com/vinicius.ferreira.58173000" target="_blank"><img src="imgs/facebook.png" alt=""></a></div>
+                    <div id="instagramicon" class="headericons"><a href="https://www.instagram.com/vb0ny/" target="_blank"><img src="imgs/instagram.png" alt=""></a></div>
+                    <div id="linkedinicon" class="headericons"><a href="https://www.linkedin.com/in/vinicius-de-assis-ferreira-309487186/" target="_blank"><img src="imgs/linkedin.png" alt=""></a></div>
+                    <div id="githubicon" class="headericons"><a href="https://github.com/vbony" target="_blank"><img src="imgs/github.png" alt=""></a></div>
+                    <div id="email" class="headericons"><a href="http://" target="_blank"><img src="imgs/mail.png" alt=""></a></div>
+                </div>
+
+                
+            </div>
+
+            <div id="headerbtns">
+                <div id="buttoncontact" onclick="dicWpp()">
+                    <div id="iconbtncontact"><img src="imgs/whatsapp.png" alt=""></div>
+                    <div id="txtbtncontact">VAMOS CONVERSAR</div>
+                </div>
+                <div id="buttoncv" onclick="dicCv()">
+                    <div id="iconbtncv"><img src="imgs/resume.png" alt=""></div>
+                    <div id="txtbtncv">BAIXAR CURRÍCULO</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="contentbackground" class="container-fluid row">
+        <div id="grid1">
+            <div id="sobremim">
+                <h2>Sobre mim</h2>
+                <p>
+                    t is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking
+                    at its layout. The point of using Lorem Ipsum is that it has
+                    a more-or-less normal distribution of letters, as opposed
+                    to using
+                </p>
+            </div>
+        </div>
+
+        <div id="grid2">
+            <div id="localemail">
+                <div id="linhaendereco">
+                    <div id="linhaenderecoimg"><img src="imgs/placeholder.png" alt=""></div>
+                    <div id="linhaenderecotxt">     <p>Brasília, DF </p>    </div>
+                </div>
+
+                <div id="linhaemail">
+                    <div id="linhaemailimg"><img src="imgs/envelope.png" alt=""></div>
+                    <div id="linhaemailtxt">    <p>contato.vinicius-af@outlook.com</p>     </div>
+                </div>
+
+                <div id="linhafone">
+                    <div id="linhafoneimg"><img src="imgs/phone.png" alt=""></div>
+                    <div id="linhafonetxt"><p>(61) 9.9951-1208</p></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+    
+</body>
+</html>
---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function dicWpp(){
+    window.open("https://api.whatsapp.com/send?phone=5561999511208&text=Olá%2c%20eu%20tenho%20interesse%20em%20te%20contratar%21&source=&data=");
+}
+
+function dicCv(){
+    window.open("imgs/vinicius_de_assis_cv.pdf");
+}
+
